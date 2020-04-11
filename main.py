8117d16fe2e8a37792c556c8aa4b8a8306f7fbff
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-04-11 17:18:15 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from auth import TOKEN
+from telegram.ext import CallbackContext, CommandHandler
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
+def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    update.message.reply_text(f'Я получил сообщение "{update.message.text}"')
+
+
+def main():
+    # Создаём объект updater.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    REQUEST_KWARGS = {
+        'proxy_url': 'socks5://96.96.33.133:1080',  # Адрес прокси сервера
+        # Опционально, если требуется аутентификация:
+        # 'urllib3_proxy_kwargs': {
+        #     'assert_hostname': 'False',
+        #     'cert_reqs': 'CERT_NONE'
+        #     'username': 'user',
+        #     'password': 'password'
+        # }
+    }
+
+    updater = Updater(TOKEN, use_context=True,
+                      request_kwargs=REQUEST_KWARGS)
+
+    # Получаем из него диспетчер сообщений.
+    dp = updater.dispatcher
+
+    # Создаём обработчик сообщений типа Filters.text
+    # из описанной выше функции echo()
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(Filters.text, echo)
+
+    # Регистрируем обработчик в диспетчере.
+    dp.add_handler(text_handler)
+    # Запускаем цикл приема и обработки сообщений.
+
+    updater.start_polling()
+
+    # Ждём завершения приложения.
+    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
+    updater.idle()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()

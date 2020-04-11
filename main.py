7f0caf039c31cf03f4de664261a0a989d32c390a
Леонид Литвинов <leonidlitvinoff@yandex.ru>
2020-04-11 17:33:36 +0700

    Task three

--- main.py
+++ main.py
@@ -6,23 +6,48 @@ from datetime import datetime
 
 # Определяем функцию-обработчик сообщений.
 # У неё два параметра, сам бот и класс updater, принявший сообщение.
-def echo(update, context):
-    # У объекта класса Updater есть поле message,
-    # являющееся объектом сообщения.
-    # У message есть поле text, содержащее текст полученного сообщения,
-    # а также метод reply_text(str),
-    # отсылающий ответ пользователю, от которого получено сообщение.
-    update.message.reply_text(f'Я получил сообщение "{update.message.text}"')
+def set_timer(update, context):
+    """Добавляем задачу в очередь"""
+    chat_id = update.message.chat_id
+    try:
+        # args[0] должен содержать значение аргумента (секунды таймера)
+        due = int(context.args[0])
+        if due < 0:
+            update.message.reply_text(
+                'Извините, не умеем возвращаться в прошлое')
+            return
 
-def time(update, context):
-    update.message.reply_text(
-        str(datetime.now().time()))
+        # Добавляем задачу в очередь
+        # и останавливаем предыдущую (если она была)
+        if 'job' in context.chat_data:
+            old_job = context.chat_data['job']
+            old_job.schedule_removal()
+        new_job = context.job_queue.run_once(task, due, context=chat_id)
+        # Запоминаем созданную задачу в данных чата.
+        context.chat_data['job'] = new_job
+        # Присылаем сообщение о том, что всё получилось.
+        update.message.reply_text(f'Вернусь через {due} секунд')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Использование: /set <секунд>')
 
 
-def date(update, context):
-    update.message.reply_text(
-        str(datetime.now().date()))
+def task(context):
+    job = context.job
+    context.bot.send_message(job.context, text='Вернулся!')
 
+
+def unset_timer(update, context):
+    # Проверяем, что задача ставилась
+    if 'job' not in context.chat_data:
+        update.message.reply_text('Нет активного таймера')
+        return
+    job = context.chat_data['job']
+    # планируем удаление задачи (выполнится, когда будет возможность)
+    job.schedule_removal()
+    # и очищаем пользовательские данные
+    del context.chat_data['job']
+    update.message.reply_text('Хорошо, вернулся сейчас!')
 
 
 def main():
@@ -50,12 +75,15 @@ def main():
     # После регистрации обработчика в диспетчере
     # эта функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
-    text_handler = MessageHandler(Filters.text, echo)
 
     # Регистрируем обработчик в диспетчере.
-    dp.add_handler(CommandHandler("date", date))
-    dp.add_handler(CommandHandler("time", time))
-    dp.add_handler(text_handler)
+    dp.add_handler(CommandHandler("set", set_timer,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
+    dp.add_handler(CommandHandler("unset", unset_timer,
+                                  pass_chat_data=True)
+                   )
     # Запускаем цикл приема и обработки сообщений.
 
     updater.start_polling()
